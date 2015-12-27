fefca534431496b58524233afc277754fdf4776d
Jani Kibichi <janikibichi@gmail.com>
2015-12-27 14:15:52 -0500

    episodes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :mentors
-  resources :mentors, only: [:index, :show]
+
+  resources :mentors, only: [:index, :show] do
+   resources :episodes
+  end
+
+  authenticated :mentor do
+   root 'mentors#dashboard', as: "authenticated_root"
+  end
 
   resources :how, only: [:index]
 
